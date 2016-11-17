3d426a39ac31cb2aadfedc2bae2a4fd5ddf01c52
Diego Reiriz Cores <diegoreiriz@gmail.com>
2016-11-17 11:43:13 +0100

    SP

--- DataSet.py
+++ DataSet.py
@@ -16,11 +16,23 @@ class DataSet:
     def value(self):
         del self._value
 
+
+    """
+        This Dataset is for a Lower triangular matrix
+    """
+    #TODO: this should be a property¿?
     def getValueXY(self,x,y):
         if x > y:
             return int(self._value[x][y])
         else:
             return int(self._value[y][x])
+
+    def setValueXY(self,x,y,value):
+        if x > y:
+            self._value[x][y] = value
+        else:
+            self._value[y][x] = value
+
 
     def __init__(self, value=None):
         if value is None:
